3e28f6d9687df0270da30c2e0642a13602086816
Sai Ananya Channamsetti <saiananyac@icloud.com>
2024-05-16 19:39:18 -0400

    index updates

---
+++ index2.js
@@ -0,0 +1,3 @@
+var collegeOptions = {"Public Trade School": 9000, "Private Trade School": 15000, "Community College": 1, "In-State Public University": 15000, "Out-of-State Public University": 32000, "Private University": 35000 }; 
+var housing = {"Apartment (monthly rent)": 800, "House (buy)": 300000, "Penthouse (buy)": 1000000}
+var lifeEvents;
