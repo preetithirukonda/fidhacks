802a6c0ddce0bb153019cc90d37cee39d67201a4
pathiruk <pathiruk@ncsu.edu>
2024-05-17 10:31:01 -0400

    added all of the new game.js code from website setup

--- game.js
+++ game.js
@@ -40,8 +40,17 @@ function eighteen(){
     document.body.innerHTML = `
     <div class="container">
     <p>Bank Balance: ${bankBalance}</p>
-        <a>Before going to college, take this quiz to learn about budgeting.</a>
-        
+        <p>You are about to graduate high school. It's time to decide where you will go to college.</p>
+        <a href="quiz.html" class="btn" target="_blank">Before going to college, take this quiz to learn about budgeting</a>
+        <a href="#" class="btn" onclick="gotocollege()">Click here when you pass the quiz to college.</a>
+    </div>
+`;
+}
+
+function gotocollege() {
+    document.body.innerHTML = `
+    <div class="container">
+    <p>Bank Balance: ${bankBalance}</p>
         <h1>Make a decision:</h1>
         <p> Go to community college: $10</p>
         <p> Go to in state public college: $100</p>
@@ -56,11 +65,19 @@ function eighteen(){
 function community(){
     bankBalance=bankBalance-10;
     cc_salary = 2000;
+    scoreVal = localStorage.getItem('score');
     document.body.innerHTML = `
     <div class="container">
     <p>Bank Balance: ${bankBalance}</p>
-        
+    <a href="collegequiz.html" class="btn" target="_blank">Now that you are in college, take this quiz about paying for college.</a>
+    <a href="#" class="btn" onclick="graduate()">Click here when you pass the quiz to graduate and get a job.</a>
+    </div>
+`;
+}
 
+function graduate(){
+    document.body.innerHTML = `
+    <div class="container">
         <p>Congrats! You have graduated from college and got your first job! Your salary is: ${cc_salary}</p>
         <p>Your bank balance is now: ${bankBalance=bankBalance+cc_salary}</p>
         <a href="#" class="btn" onclick="chap2housing()">Click here to choose housing</a>
@@ -74,10 +91,8 @@ function instate() {
     document.body.innerHTML = `
     <div class="container">
     <p>Bank Balance: ${bankBalance}</p>
-
-        <p>Congrats! You have graduated from college and got your first job! Your salary is: ${public_salary}</p>
-        <p>Your bank balance is now: ${bankBalance=bankBalance+public_salary}</p>
-        <a href="#" class="btn" onclick="chap2housing()">Click here to choose housing</a>
+    <a href="collegequiz.html" class="btn" target="_blank">Now that you are in college, take this quiz about paying for college.</a>
+    <a href="#" class="btn" onclick="graduate()">Click here when you pass the quiz to graduate and get a job.</a>
     </div>
 `;
 }
@@ -88,15 +103,31 @@ function privateschool () {
     document.body.innerHTML = `
     <div class="container">
     <p>Bank Balance: ${bankBalance}</p>
-
-        <p>Congrats! You have graduated from college and got your first job! Your salary is: ${private_salary}</p>
-        <p>Your bank balance is now: ${bankBalance=bankBalance+private_salary}</p>
-        <a href="#" class="btn" onclick="chap2housing()">Click here to choose housing</a>
+    <a href="collegequiz.html" class="btn" target="_blank">Now that you are in college, take this quiz about paying for college.</a>
+    <a href="#" class="btn" onclick="graduate()">Click here when you pass the quiz to graduate and get a job.</a>
     </div>
 `;
 }
 
 function chap2housing() {
+    document.body.innerHTML = `
+    <div class="container">
+    <p>Bank Balance: ${bankBalance}</p>
+    <a href="housingquiz.html" class="btn" target="_blank">It's time to buy a home! Before you do, take this quiz on housing finances.</a>
+    <a href="#" class="btn" onclick="selecthome()">Click here when you pass the quiz to select a home.</a>
+
+        <h1>Make a decision:</h1>
+        <p> Buy an apartment: $800</p>
+        <p> Buy a house: $5000 </p>
+        <p> Buy a penthouse: $9000</p>
+        <a href="#" class="btn" onclick="apartment()">Apartment</a>
+        <a href="#" class="btn" onclick="house()">House</a>
+        <a href="#" class="btn" onclick="penthouse()">Penthouse</a>
+    </div>
+`;
+}
+
+function selecthome() {
     document.body.innerHTML = `
     <div class="container">
     <p>Bank Balance: ${bankBalance}</p>
@@ -116,7 +147,7 @@ function apartment() {
     document.body.innerHTML = `
     <div class="container">
     <p>Bank Balance: ${bankBalance}</p>
-
+    <p>Congrats! You bought your apartment.</p>
         <a href="#" class="btn" onclick="yearslater()">Advance 45 years</a>
     </div>
 `;
@@ -127,7 +158,7 @@ function house() {
     document.body.innerHTML = `
     <div class="container">
     <p>Bank Balance: ${bankBalance}</p>
-
+    <p>Congrats! You bought your house.</p>
         <a href="#" class="btn" onclick="yearslater()">Advance 45 years</a>
     </div>
 `;
@@ -138,13 +169,22 @@ function penthouse() {
     document.body.innerHTML = `
     <div class="container">
     <p>Bank Balance: ${bankBalance}</p>
-
+    <p>Congrats! You bought your penthouse.</p>
         <a href="#" class="btn" onclick="yearslater()">Advance 45 years</a>
     </div>
 `;
 }
 
 function yearslater() {
+    document.body.innerHTML = `
+    <div class="container">
+    <a href="retirementquiz.html" class="btn" target="_blank">It's time to retire. Take this quiz before retirement.</a>
+    <a href="#" class="btn" onclick="retire()">Click here when you pass the quiz to retire.</a>
+    </div>
+`;
+}
+
+function retire() {
     document.body.innerHTML = `
     <div class="container">
     <p>Your total bank balance is: ${bankBalance}</p>
